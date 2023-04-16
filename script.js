9f02346a1078c3b4470df37ccb8283a0be582f50
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-16 23:25:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let input=prompt();
+let arr={};
+for(let i=0;i<input.length;i++){
+	if(arr[input.charAt(i)]!=undefined){
+		arr[input.charAt(i)]=arr[input.charAt(i)]++;
+	}else{
+		arr[input.charAt(i)]=1;
+	}
+}
+
+for (let i = 0; i < input.length; i++){
+  if (arr[input.charAt(i)] == 1) {
+    console.log(input.charAt(i));
+    break;
+  }
+}
+
